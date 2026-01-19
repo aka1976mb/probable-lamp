18012a2fa77201391950a5dc9ddb13579b5d338e
MUSTAPHA BARKI <startexportsarlma@outlook.com>
2026-01-19 20:36:12 +0000

    Initialize dev

---
+++ README.md
@@ -0,0 +1,31 @@
+# nocodb-to-gpt-via-api
+
+## Development Instructions
+
+> [!TIP]
+> This project utilizes [`uv`](https://docs.astral.sh/uv/) for module management.
+> You can find installation instructions via [relevant operating system](https://docs.astral.sh/uv/getting-started/installation/) documentation.
+
+- Clone: `gh repo clone aksbdc/nocodb-to-gpt-via-api`
+- Source: `cd nocodb-to-gpt-via-api`
+- Build: [`make`](https://www.gnu.org/software/make/)
+
+### Branch Management
+
+- `main` → production ready environment.
+- `staging` → released changes to be merged into `main`.
+- `dev` → testing changes to be merged into `staging`.
+
+## [Architecture Diagram](https://github.com/aksbdc/nocodb-to-gpt-via-api/wiki)
+
+## Scenario Planning
+
+1. [Small Business Owner](https://aksbdc.org/success-stories/)
+1. [Expert Advisor](https://aksbdc.org/about/advisors/)
+1. [General Public](https://alaska.gov/)
+
+### [System Benchmarking](docs/ANALYSIS.md)
+
+### [Experimental Design](docs/ARCHITECTURE.md)
+
+### [Research Question](docs/NOTES.md)
---
+++ main.py
@@ -0,0 +1,67 @@
+from dotenv import load_dotenv
+import requests
+import json
+import os
+
+load_dotenv()
+
+xc_token = os.getenv("API_KEY")
+base_name = os.getenv("BASE_NAME")
+table_name = os.getenv("TABLE_NAME")
+
+OUTPUT = "data/response.json"
+URL = f"https://app.nocodb.com/api/v3/data/{base_name}/{table_name}/records"
+
+HEADERS = {
+    "accept": "application/json",
+    "user-agent": "aksbdc/nocodb-to-gpt-via-api/0.2.1",
+    "xc-token": xc_token,
+}
+
+PARAMETERS = {}
+
+
+def project_description():
+    """
+    Example entrypoint for command-line interface (CLI).
+    """
+    print(">> Hello from nocodb-to-gpt-via-api!")
+    print(">> This is a simple script that fetches data from an instance via the API.")
+    print(">> The data is then used to train a GPT model.")
+
+
+def fetch_data(location):
+    """
+    Retrieve data from the instance.
+    """
+    try:
+        response = requests.request("GET", URL, headers=HEADERS, params=PARAMETERS)
+
+        response.raise_for_status()
+
+        data = response.json()
+
+        os.makedirs(os.path.dirname(location), exist_ok=True)
+
+        with open(location, "w", encoding="utf-8") as file:
+            json.dump(data, file, ensure_ascii=False, indent=4)
+
+        print(f">> Captured output: {location}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"[!] Error w/ API request: {e}")
+
+    except ValueError as e:
+        print(f"[?] Error w/ JSON response parsing: {e}")
+
+    except IOError as e:
+        print(f"[*] Error w/ file write: {e}")
+
+
+def main():
+    project_description()
+    fetch_data(OUTPUT)
+
+
+if __name__ == "__main__":
+    main()
